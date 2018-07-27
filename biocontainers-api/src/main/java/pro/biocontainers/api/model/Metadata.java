ec2b98e42e1003a4f60d5f7f25bfe669544a2662
Chakradhar Bandla <chakrabandla@gmail.com>
2018-07-27 14:53:08 +0900

    swagger docs : complete documentation

--- biocontainers-api/src/main/java/pro/biocontainers/api/model/Metadata.java
+++ biocontainers-api/src/main/java/pro/biocontainers/api/model/Metadata.java
@@ -1,6 +1,8 @@
 package pro.biocontainers.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,25 +11,33 @@ import lombok.Data;
  */
 @Data
 @Builder
+@ApiModel(description = "Describes this registry to better allow for mirroring and indexing.")
 public class Metadata {
+    @ApiModelProperty(required = true, value = "The version of this registry")
     @JsonProperty("version")
     private String version;
 
+    @ApiModelProperty(required = true, value = "The version of the GA4GH tool-registry API supported by this registry")
     @JsonProperty("api_version")
     private String apiVersion;
 
+    @ApiModelProperty(value = "A country code for the registry (ISO 3166-1 alpha-3)")
     @JsonProperty("country")
     private String country;
 
+    @ApiModelProperty(value = "A friendly name that can be used in addition to the hostname to describe a registry")
     @JsonProperty("friendly_name")
     private String friendlyName;
 
+    @ApiModelProperty(value = "Brief descrption")
     @JsonProperty("description")
     private String description;
 
+    @ApiModelProperty(value = "Contact details")
     @JsonProperty("contact")
     private Contact contact;
 
+    @ApiModelProperty(value = "license")
     @JsonProperty("license")
     private String license;
 }
--- biocontainers-api/src/main/java/pro/biocontainers/api/model/ToolClass.java
+++ biocontainers-api/src/main/java/pro/biocontainers/api/model/ToolClass.java
@@ -1,19 +1,25 @@
 package pro.biocontainers.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * Describes a class (type) of tool allowing us to categorize workflows, tasks, and maybe even other entities (such as services) separately
  */
 @Data
+@ApiModel(description = "Describes a class (type) of tool allowing us to categorize workflows, tasks, and maybe even other entities (such as services) separately")
 public class ToolClass {
+    @ApiModelProperty(value = "The unique identifier for the class")
     @JsonProperty("id")
     private String id;
 
+    @ApiModelProperty(value = "A short friendly name for the class")
     @JsonProperty("name")
     private String name;
 
+    @ApiModelProperty(value = "A longer explanation of what this class is and what it can accomplish")
     @JsonProperty("description")
     private String description;
 }
