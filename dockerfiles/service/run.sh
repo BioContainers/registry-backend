87977e745f05b20e1550e7250189b7a4e5a00fb2
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-09-14 22:51:05 +0100

    Improvements and clarifications for authentication

--- dockerfiles/service/run.sh
+++ dockerfiles/service/run.sh
@@ -1,8 +1,12 @@
 #!/bin/env bash
 
-java -jar /api-service.jar --mongodb.biocontainers.db.database=$MONGO_DB \
+#   --mongodb.biocontainers.db.authenticationDatabase= \
+#  --mongodb.biocontainers.db.port= \
+
+mongo --host $MONGODB_HOST -u root -p $MONGO_ROOT_PASS --authenticationDatabase $MONGODB_ADMIN_DB --eval "db.grantRolesToUser( $MONGODB_USER , [ { 'role' : 'readWrite', 'db' : $BIOCONT_DB_NAME } ] )"
+
+java -jar /api-service.jar --mongodb.biocontainers.db.database=$BIOCONT_DB_NAME \
   --mongodb.biocontainers.db.user=$MONGODB_USER \
   --mongodb.biocontainers.db.password=$MONGODB_PASS \
-  --mongodb.biocontainers.db.authenticationDatabase= \
-  --mongodb.biocontainers.db.port= \
+  --mongodb.biocontainers.db.authenticationDatabase=$MONGODB_ADMIN_DB \
   --mongodb.biocontainers.db.host=$MONGODB_HOST \
