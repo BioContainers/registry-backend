4854cd91d61766aa3e1e2b032c284f816759933a
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-09-23 23:01:55 +0100

    Minor cleaning and correct setup of port for listening in chart.

--- dockerfiles/service/run.sh
+++ dockerfiles/service/run.sh
@@ -8,15 +8,17 @@ db = db.getSiblingDB('admin');
 try {
   db.createUser({ user: "$MONGODB_USER" , pwd: "$MONGODB_PASS", roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"] });
 } catch(err) {
-  print("Could not create user, possibly exists:"+err.message )
+  print("Could not create user: "+err.message )
   print("Trying to add role to existing user")
   db.grantRolesToUser( "$MONGODB_USER", ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"] );
 }
+/* uncomment for debugging
 printjson( db.getUser( "$MONGODB_USER", {
    showCredentials: false,
    showPrivileges: true,
    showAuthenticationRestrictions: true
 } ) )
+*/
 EOM
 
 cat mongo_exec.js
@@ -24,9 +26,6 @@ cat mongo_exec.js
 
 mongo --host $MONGODB_HOST -u root -p $MONGO_ROOT_PASS --authenticationDatabase \
   $MONGODB_ADMIN_DB mongo_exec.js
-
-  #--eval \
-  #"db.grantRolesToUser( $MONGODB_USER , [ { 'role' : 'readWrite', 'db' : $BIOCONT_DB_NAME } ] )"
 
 java -jar /api-service.jar --mongodb.biocontainers.db.database=$BIOCONT_DB_NAME \
   --mongodb.biocontainers.db.user=$MONGODB_USER \
